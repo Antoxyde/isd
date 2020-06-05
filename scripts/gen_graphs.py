61fcb99aef72f81b20af81954b1b88210b20e258
antox <antox@w0nderland.fr>
2020-06-05 08:58:02 +0200

    graph prange runs okay

--- scripts/gen_graphs.py
+++ scripts/gen_graphs.py
@@ -1,2 +1,26 @@
 import glob
-from
+import matplotlib.pyplot as plt
+import re
+
+
+REG = re.compile('niter=(\d+), time=(\d+).(\d+), wt=(\d{3})')
+
+def load_run(runfile):
+    xi,yi = [],[]
+    with open(runfile, "r") as fd:
+        for line in fd.read().strip().split("\n"):
+            _, time, _, wt = REG.search(line).groups()
+            xi.append(int(time))
+            yi.append(int(wt))
+    return xi,yi
+
+for runfile in glob.glob("runs/prange*"):
+    plt.plot(*load_run(runfile))
+
+plt.xlabel("Time in second")
+plt.ylabel("Hamming weigth")
+plt.title("Prange runs (24h each)")
+plt.show()
+
+
+

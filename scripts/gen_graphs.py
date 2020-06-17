aabba31d8193d0608169471b72f8d0f17b24c508
antox <git@w0nderland.fr>
2020-06-17 15:01:40 +0200

    Added 48h prange runs

--- scripts/gen_graphs.py
+++ scripts/gen_graphs.py
@@ -8,19 +8,28 @@ def load_run(runfile):
     xi,yi = [],[]
     with open(runfile, "r") as fd:
         for line in fd.read().strip().split("\n"):
-            _, time, _, wt = REG.search(line).groups()
-            xi.append(int(time))
-            yi.append(int(wt))
+            if not any([line.startswith('#'), line.startswith('0'), line.startswith('1')]):
+                _, time, _, wt = REG.search(line).groups()
+                xi.append(int(time))
+                yi.append(int(wt))
     return xi,yi
 
-for runfile in glob.glob("runs/prange*"):
+for runfile in glob.glob("runs/prange_24_*"):
     plt.plot(*load_run(runfile))
 
 plt.xlabel("Time in second")
 plt.ylabel("Hamming weigth")
 plt.title("Prange runs (24h each)")
-plt.ylim(220,240)
+plt.ylim(220,260)
 plt.show()
 
+for runfile in glob.glob("runs/prange_48_*"):
+    plt.plot(*load_run(runfile))
+
+plt.xlabel("Time in second")
+plt.ylabel("Hamming weigth")
+plt.title("Prange runs (48h each)")
+plt.ylim(220,260)
+plt.show()
 
 

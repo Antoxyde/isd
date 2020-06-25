f5b5a91a0ba4912708ae1071f81383cfe5db982b
antox <git@w0nderland.fr>
2020-06-25 11:09:21 +0200

    avancement #15 + fix #19

--- stern.c
+++ stern.c
@@ -8,6 +8,9 @@
 #define STERN_GET(tab, index) ((tab[index >> 6]) >> (index & 0x3f)) & 1
 #define STERN_SET_ONE(tab, index) (tab[index >> 6]) |= (1ULL << (index & 0x3f))
 
+#define N 1280
+#define K 640
+
 mzd_t* isd_stern_canteaut_chabaud_p2_sort(mzd_t* G, uint64_t time_sec, uint64_t sigma, uint64_t radix_width, uint64_t radix_nlen, uint64_t m, uint64_t c) {
 
     // Time mesuring stuff
@@ -17,14 +20,13 @@ mzd_t* isd_stern_canteaut_chabaud_p2_sort(mzd_t* G, uint64_t time_sec, uint64_t 
     // p is the Stern p parameter. (= number of LC to make for each rows subsets)
     uint64_t p = 2, iter = 0, mwin = 0, delta = 0, citer = 0;
 
-    rci_t n = G->ncols, comb1[2 /* p */], comb2[2 /* p */], min_comb[4 /* 2*p */],lambda = 0, mu = 0, tmp = 0, i = 0, j = 0;
-    rci_t k = n/2; // number of rows in G, as it generates a [n, n/2] code.
+    rci_t comb1[2 /* p */], comb2[2 /* p */], min_comb[4 /* 2*p */],lambda = 0, mu = 0, tmp = 0, i = 0, j = 0;
 
-    int min_wt = k - 1, wt = 0;
+    int min_wt = K - 1, wt = 0;
 
 #if defined(AVX512_ENABLED)
-    uint64_t mask[10];
-    memset(mask, 0, 80);
+    uint64_t mask[10] /* K/64 */;
+    memset(mask, 0, 80 /* K/8 */);
 #endif
 
 #if defined(DEBUG)
@@ -32,28 +34,28 @@ mzd_t* isd_stern_canteaut_chabaud_p2_sort(mzd_t* G, uint64_t time_sec, uint64_t 
 #endif
 
     uint64_t* word = NULL;
-    uint64_t* linear_comb = (uint64_t*)malloc(sizeof(uint64_t) * 10);
-    uint64_t* linear_comb_next = (uint64_t*)malloc(sizeof(uint64_t) * 10);
-    uint64_t* min_cw = (uint64_t*)malloc(sizeof(uint64_t) * 10);
-    rci_t* column_perms_copy =  (rci_t*) malloc(sizeof(rci_t) * n);
-    rci_t* column_perms = (rci_t*) malloc(sizeof(rci_t) * n);
+    uint64_t* linear_comb = (uint64_t*)malloc(sizeof(uint64_t) * 10 /* K/64 */);
+    uint64_t* linear_comb_next = (uint64_t*)malloc(sizeof(uint64_t) * 10 /* K/64 */);
+    uint64_t* min_cw = (uint64_t*)malloc(sizeof(uint64_t) * 10 /* K/64 */);
+    rci_t* column_perms_copy =  (rci_t*) malloc(sizeof(rci_t) * N);
+    rci_t* column_perms = (rci_t*) malloc(sizeof(rci_t) * N);
     CHECK_MALLOC(linear_comb);
     CHECK_MALLOC(linear_comb_next);
     CHECK_MALLOC(min_cw);
     CHECK_MALLOC(column_perms);
     CHECK_MALLOC(column_perms_copy);
 
-    for (i = 0; i < n; i++) column_perms[i] = i;
+    for (i = 0; i < N; i++) column_perms[i] = i;
 
     mzd_t* Gtemp = mzd_copy(NULL, G);
 
     // Ensure that we work with a systematic generator matrix
     rref_to_systematic(Gtemp, column_perms);
-    mzd_t* Glw = mzd_submatrix(NULL, Gtemp, 0, k, k, n);
+    mzd_t* Glw = mzd_submatrix(NULL, Gtemp, 0, K, K, N);
 
     // nelem is the max number of LCs we will make
     // so its an upper bound for the size of each array
-    uint32_t nelem = ((n/4 * (n/4 - 1)) /2);
+    uint32_t nelem = ((N/4 * (N/4 - 1)) /2);
 
     // Number of possible dfferent values for delta
     uint32_t nb_keys = 1UL << sigma;
@@ -125,13 +127,13 @@ mzd_t* isd_stern_canteaut_chabaud_p2_sort(mzd_t* G, uint64_t time_sec, uint64_t 
             /* Start of the Canteaut-Chabaud stuff, to derive a new information set. */
 
             // Find lambda, mu s.t. Glw[lambda, mu] == 1
-            lambda = xoshiro256starstar_random() % k;
+            lambda = xoshiro256starstar_random() % K;
             word = Glw->rows[lambda];
 
-            mu = xoshiro256starstar_random() % 10;
+            mu = xoshiro256starstar_random() % 10 /* K/64 */
             // Assuming a whole row can't be zero
             while (word[mu] == 0) {
-                mu = (mu + 1) % 10;
+                mu = (mu + 1) % 10 /* K/64 */;
             }
 
             // We need to take a random 1 in this word.
@@ -147,8 +149,8 @@ mzd_t* isd_stern_canteaut_chabaud_p2_sort(mzd_t* G, uint64_t time_sec, uint64_t 
 
            // Log the column swapping
             tmp = column_perms[lambda];
-            column_perms[lambda] = column_perms[mu + 640 /* k */];
-            column_perms[mu + 640 /* k */] = tmp;
+            column_perms[lambda] = column_perms[mu + 640 /* K */];
+            column_perms[mu + 640 /* K */] = tmp;
 
             // Clear the bit at the intersection of the lambda'th row and the mu'th column
             // so we don't have to rewrite a 1 in mu'th column everytime we add the lambda'th row
@@ -165,7 +167,7 @@ mzd_t* isd_stern_canteaut_chabaud_p2_sort(mzd_t* G, uint64_t time_sec, uint64_t 
 #endif
 
             // Add the lambda'th row to every other row that have a 1 in the mu'th column
-            for (j = 0; j < k; j++) {
+            for (j = 0; j < K; j++) {
 
 
 #if defined(AVX512_ENABLED)
@@ -193,7 +195,7 @@ mzd_t* isd_stern_canteaut_chabaud_p2_sort(mzd_t* G, uint64_t time_sec, uint64_t 
 
 #if defined(AVX512_ENABLED)
             // Clear the mask for the next iteration
-            mask[mu/64] = 0;
+            mask[big_mu] = 0;
 #endif
         }
 
@@ -209,11 +211,11 @@ mzd_t* isd_stern_canteaut_chabaud_p2_sort(mzd_t* G, uint64_t time_sec, uint64_t 
         }
 
         // 1st pass, gen all the LC from the first k/2 rows
-        for (comb1[0] = 0; comb1[0]  < 320 /* n/4 */; comb1[0]++) {
+        for (comb1[0] = 0; comb1[0]  < 320 /* K/2 */; comb1[0]++) {
 
             uint64_t* row1 = (uint64_t*)Glw->rows[comb1[0]];
 
-            for (comb1[1] = comb1[0] + 1; comb1[1] < n/4; comb1[1]++) {
+            for (comb1[1] = comb1[0] + 1; comb1[1] < 320 /* K/2 */; comb1[1]++) {
 
                 uint64_t* row2 = (uint64_t*)Glw->rows[comb1[1]];
 
@@ -240,11 +242,11 @@ mzd_t* isd_stern_canteaut_chabaud_p2_sort(mzd_t* G, uint64_t time_sec, uint64_t 
 
         // 2nd pass, gen all the LC from the k/2 to k rows but store
         // only the ones that will collides with at least 1 element from lc_tab_first
-        for (comb2[0] = 320 /* n/4 */; comb2[0]  < 640 /* n/2 */; comb2[0]++) {
+        for (comb2[0] = 320 /* K/2 */; comb2[0]  < 640 /* K */; comb2[0]++) {
 
             uint64_t* row1 = (uint64_t*)Glw->rows[comb2[0]];
 
-            for (comb2[1] = comb2[0] + 1; comb2[1] < 640 /* n/2 */; comb2[1]++) {
+            for (comb2[1] = comb2[0] + 1; comb2[1] < 640 /* K */; comb2[1]++) {
 
                 uint64_t* row2 = (uint64_t*)Glw->rows[comb2[1]];
 
@@ -322,14 +324,14 @@ mzd_t* isd_stern_canteaut_chabaud_p2_sort(mzd_t* G, uint64_t time_sec, uint64_t 
                 void* row2 = (void*)Glw->rows[lc_tab_alias_second_sorted[mwin][index_second].index2];
 
                 __m512i linear_comb_high = _mm512_loadu_si512(row1);
-                __m128i linear_comb_low = _mm_loadu_si128(row1 + 64);
+                __m128i linear_comb_low = _mm_loadu_si128(row1 + 64 );
 
                 linear_comb_high = _mm512_xor_si512(linear_comb_high, _mm512_loadu_si512(row2));
                 linear_comb_low = _mm_xor_si128(linear_comb_low, _mm_loadu_si128(row2 + 64));
 #else
-                memset(linear_comb, 0, 80);
-                mxor(linear_comb, (uint64_t*)Glw->rows[lc_tab_alias_second_sorted[mwin][index_second].index1], 10);
-                mxor(linear_comb, (uint64_t*)Glw->rows[lc_tab_alias_second_sorted[mwin][index_second].index2], 10);
+                memset(linear_comb, 0, 80 /* K/8 */);
+                mxor(linear_comb, (uint64_t*)Glw->rows[lc_tab_alias_second_sorted[mwin][index_second].index1], 10 /* K/64 */);
+                mxor(linear_comb, (uint64_t*)Glw->rows[lc_tab_alias_second_sorted[mwin][index_second].index2], 10 /* K/64 */);
 #endif
 
                 for (index_third = save_index_third; index_third < lc_tab_third_size[mwin] && lc_tab_alias_second_sorted[mwin][index_second].delta == lc_tab_alias_third_sorted[mwin][index_third].delta; index_third++) {
@@ -355,9 +357,9 @@ mzd_t* isd_stern_canteaut_chabaud_p2_sort(mzd_t* G, uint64_t time_sec, uint64_t 
                     _mm_storeu_si128((__m128i*)(linear_comb_next + 8), linear_comb_low_next);
 
 #else
-                    memcpy(linear_comb_next, linear_comb, 80);
-                    mxor(linear_comb_next, (uint64_t*)Glw->rows[lc_tab_alias_third_sorted[mwin][index_third].index1], 10);
-                    mxor(linear_comb_next, (uint64_t*)Glw->rows[lc_tab_alias_third_sorted[mwin][index_third].index2], 10);
+                    memcpy(linear_comb_next, linear_comb, 80 /* K/8 */ );
+                    mxor(linear_comb_next, (uint64_t*)Glw->rows[lc_tab_alias_third_sorted[mwin][index_third].index1], 10 /* K/64 */);
+                    mxor(linear_comb_next, (uint64_t*)Glw->rows[lc_tab_alias_third_sorted[mwin][index_third].index2], 10 /* K/64 */);
 #endif
 
                     //printf("DBG mwin = %lu, linear comb is : \n", mwin);
@@ -380,8 +382,8 @@ mzd_t* isd_stern_canteaut_chabaud_p2_sort(mzd_t* G, uint64_t time_sec, uint64_t 
                         min_comb[2] = lc_tab_alias_third_sorted[mwin][index_third].index1;
                         min_comb[3] = lc_tab_alias_third_sorted[mwin][index_third].index2;
 
-                        memcpy(min_cw, linear_comb_next, 80);
-                        memcpy(column_perms_copy, column_perms, n * sizeof(rci_t));
+                        memcpy(min_cw, linear_comb_next, 80 /* K/8 */);
+                        memcpy(column_perms_copy, column_perms, N * sizeof(rci_t));
 
                         mzd_t* cw = stern_reconstruct_cw(min_comb, column_perms_copy, min_cw, p);
                         print_cw(cw);

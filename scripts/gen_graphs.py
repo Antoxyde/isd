f0bde25e379b500c6d0e64e001a2daeb059d41ae
antox <git@w0nderland.fr>
2020-07-03 16:43:38 +0200

    modif stats gen_graphs

--- scripts/gen_graphs.py
+++ scripts/gen_graphs.py
@@ -14,14 +14,45 @@ def load_run(runfile):
                 yi.append(int(wt))
     return xi,yi
 
-for runfile in glob.glob("runs/stern_48h*"):
-    plt.plot(*load_run(runfile))
 
-plt.xlabel("Time in second")
-plt.ylabel("Hamming weigth")
-plt.title("Stern runs (48h each)")
+def get_average_at(runs, t):
 
-plt.ylim(220,260)
-plt.show()
+    som = 0
+    for run in runs:
+        for ti,di in zip(run[0],run[1]):
+            if ti > t :
+                som += di_last
+                break
+            elif ti == run[0][-1]:
+                som += di
+                break
+            di_last = di
+    return som / len(runs)
 
+pranges = [load_run(runfile) for runfile in glob.glob("runs/prange_48h*")]
+stern = [load_run(runfile) for runfile in glob.glob("runs/stern_48h*")]
+stern_3pass = [load_run(runfile) for runfile in glob.glob("runs/stern_3pass_48h*")]
 
+print("Moy low prange : {}".format( sum(run[1][-1] for run in pranges)/len(pranges)))
+print("Moy low stern : {}".format( sum(run[1][-1] for run in stern)/len(stern)))
+print("Moy low stern multiwin : {}".format( sum(run[1][-1] for run in stern_3pass)/len(stern_3pass)))
+
+def plot_average_weight_per_hour():
+
+    prange_averages, stern_averages, stern_3pass_averages = [],[],[]
+    for t in range(12*48):
+        prange_averages.append(get_average_at(pranges, t*3600/12))
+        stern_averages.append(get_average_at(stern, t*3600/12))
+        stern_3pass_averages.append(get_average_at(stern_3pass, t*3600/12))
+
+    plt.plot([x/12 for x in range(0, 12*48)], prange_averages, label='Prange')
+    plt.plot([x/12 for x in range(0, 12*48)], stern_averages, label='Stern')
+    plt.plot([x/12 for x in range(0, 12*48)], stern_3pass_averages, label='Stern multi-fenêtre')
+    plt.rc('font', family='serif', size=40)
+    plt.xlabel('Time (h)', fontsize=40)
+    plt.ylabel('Hamming weight', fontsize=40)
+    plt.rcParams.update({'font.size': 40})
+    plt.legend()
+    plt.show()
+
+plot_average_weight_per_hour()

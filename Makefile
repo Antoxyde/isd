b2f4fdb211ca262f66ef7ee9e397374a596e53fa
antox <git@w0nderland.fr>
2021-05-18 14:15:16 +0200

    refactor a bit

--- Makefile
+++ Makefile
@@ -11,21 +11,22 @@ else
 endif
 
 CC=gcc
+CFLAGS=-Ofast
+#CFLAGS=-g -fsanitize=address -Wextra -Wall -Werror --std=c99 -mbmi -DDEBUG
 LDFLAGS=-lm4ri
-EXECUTABLES=main_stern main_prange
+EXECUTABLES=nns_test
 
 all: $(EXECUTABLES)
 
 %.o : %.c %.h
 	$(CC) $(CFLAGS) -c $<
 
+nns_test.o: nns_test.c utils.h libpopcnt.h 
 main_stern.o: main_stern.c stern.h utils.h libpopcnt.h xoshiro256starstar.h
 main_prange.o: main_prange.c prange.h utils.h libpopcnt.h xoshiro256starstar.h
-main_ballcoll.o: main_ballcoll.c ballcoll.h utils.h libpopcnt.h xoshiro256starstar.h
 
 prange.o: prange.c prange.h utils.h libpopcnt.h xoshiro256starstar.h
 stern.o: stern.c utils.h libpopcnt.h xoshiro256starstar.h
-ballcoll.o: ballcoll.c utils.h libpopcnt.h xoshiro256starstar.h
 
 main_stern: utils.o main_stern.o  stern.o #libm4ri-0.0.20200125.so
 	$(CC) -o $@ $^ $(CFLAGS) $(LDFLAGS)
@@ -33,10 +34,15 @@ main_stern: utils.o main_stern.o  stern.o #libm4ri-0.0.20200125.so
 main_prange: utils.o main_prange.o prange.o #libm4ri-0.0.20200125.so
 	$(CC) -o $@ $^ $(CFLAGS) $(LDFLAGS)
 
+nns_test: utils.o nns_test.o 
+	$(CC) -o $@ $^ $(CFLAGS) $(LDFLAGS)
 clean:
 	rm -f $(EXECUTABLES) *.o
 
 nns:
-	gcc -g -fsanitize=address -c utils.c -o utils.o
-	gcc -g -fsanitize=address -c nns_test.c -o nns_test.o
-	gcc -g -fsanitize=address -o nns_test nns_test.o utils.o -lasan -lm4ri
+	#gcc -g -fsanitize=address -c utils.c -o utils.o
+	#gcc -g -fsanitize=address -c nns_test.c -o nns_test.o
+	#gcc -g -fsanitize=address -o nns_test nns_test.o utils.o -lasan -lm4ri
+	gcc -c utils.c -o utils.o
+	gcc -O3 -c nns_test.c -o nns_test.o
+	gcc -O3 -o nns_test nns_test.o utils.o -lm4ri
--- nns_test.c
+++ nns_test.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <m4ri/m4ri.h>
-#include "utils.h"
 
-void test_precomp(mzd_t* H, uint64_t* H1tab, uint64_t* H2tab, uint64_t n, uint64_t k) {
-    
+#include "utils.h"
+#include "libpopcnt.h"
+
+int test_matrix_tabulation(mzd_t* H, uint64_t* H1tab, uint64_t* H2tab, uint64_t n, uint64_t k) {
+    printf("Testing matrix tabulation..\n");    
     for (uint64_t val = 0; val < (1ULL << k); val++) {
         uint64_t result = 0;
         mzd_t* vec = mzd_init(1,n);
@@ -16,57 +18,43 @@ void test_precomp(mzd_t* H, uint64_t* H1tab, uint64_t* H2tab, uint64_t n, uint64
 
         mzd_mul(res, vec, H, 0);
         uint64_t* alias_res = mzd_row(res, 0);
-        /* 
-        printf("H:\n");
-        mzd_info(H, 0);
-        mzd_print(H);
-
-        printf("vec:\n");
-        mzd_info(vec, 0);
-        mzd_print(vec);
-
-        printf("res : \n");
-        mzd_info(res, 0);
-        mzd_print(res);
-        */
 
         result = H2tab[val >> n/2] ^ H1tab[val & ((1 << n/2) - 1)];
 
         if (*alias_res != result) {
-            printf("test_precomp failed for val = %lu\n", val);
+            printf("Matrix tabulation test failed for val = %lu\n", val);
             printf("alias_res : %lx\n", *alias_res);
             printf("result     : %lx\n", result);
-            return;
+            return 0;
         }
     }
+    
+    printf("Ok.\n");
 
-    printf("test_precomp ok\n");
+    return 1;
 }
 
+int test_syndrome_table(uint64_t* SM,uint64_t* Httab1, uint64_t* Httab2, uint64_t n, uint64_t k) {
+    printf("Starting syndrome table test...\n");
 
-int main(void) {
+    // Full test should be on all 2^n vectors, but depending on n it might be too expensive
+    for (uint64_t e = 1; e < (1ULL << MIN(n, 26)); e++) { 
+        uint64_t s = Httab2[e >> n/2] ^ Httab1[e & ((1 << n/2) - 1)];
+
+        if (popcnt64_unrolled(&SM[s], n) > popcnt64_unrolled(&e, n)) {
+            printf("Syndrome table test failed; for syndrome s=%lx, a smaller error exists : %lx (SM[s] = %lx)\n", s, e, SM[s]);
+            return 0;
+        }
+    }
     
-    uint64_t n = 40, k = 20;
-    mzd_t* M = mzd_init(k, n-k);
-    mzd_t* M2 = mzd_init(k, n-k);
-    mzd_t* Ink = mzd_init(n-k,n-k);
+    printf("Ok.\n");
+    return 1;
+}
 
-    for (int i = 0; i < n-k; i++) mzd_write_bit(Ink, i, i, 1);
+/* Tabulate M s.t. xM = Mtab1[x LSBs] ^ Mtab2[x MSBs]*/
+void tabulate_matrix(mzd_t* M, uint64_t* Mtab1, uint64_t* Mtab2, uint64_t n, uint64_t k) {
     
-    do {
-        mzd_randomize(M);
-        mzd_copy(M2, M);
-    } while (mzd_echelonize(M2, 0) != k);
-
-    mzd_t* H = mzd_concat(NULL,  M, Ink); // H = [M | I_{n-k}]
-    mzd_t* Ht = mzd_transpose(NULL, H);
-
-    mzd_free(M2);
-
-    uint64_t *Ht1tab = malloc(sizeof(uint64_t) * (1ULL << n/2));
-    uint64_t *Ht2tab = malloc(sizeof(uint64_t) * (1ULL << n/2));
-    CHECK_MALLOC(Ht1tab); CHECK_MALLOC(Ht2tab);
-    
+    printf("Tabulating matrix...\n");
     mzd_t* vec_high, *vec_low, *res;
     vec_high = mzd_init(1, n);
     vec_low = mzd_init(1, n);
@@ -74,41 +62,34 @@ int main(void) {
 
     uint64_t* alias_low = mzd_row(vec_low,0);
     uint64_t* alias_high = mzd_row(vec_high,0);
-    
-    mzd_info(H, 0);
-    mzd_info(Ht, 0);
 
-    printf("H tabulation precomputation...\n");
     for (*alias_low = 0; *alias_low < (1ULL << n/2); (*alias_low)++) {
 
         *alias_high = *alias_low << n/2;
-        mzd_mul(res, vec_low, Ht, 0);
-        Ht1tab[*alias_low] = *mzd_row(res, 0);
+        mzd_mul(res, vec_low, M, 0);
+        Mtab1[*alias_low] = *mzd_row(res, 0);
 
-        mzd_mul(res, vec_high, Ht, 0);
-        Ht2tab[*alias_low] = *mzd_row(res, 0);
+        mzd_mul(res, vec_high, M, 0);
+        Mtab2[*alias_low] = *mzd_row(res, 0);
     }
-    printf("H tabulation precomputation done.\n");
 
-    uint64_t t, old;
+    printf("Done.\n");
+}
 
-    //test_precomp(Ht, Ht1tab, Ht2tab, n, k);
-
-    // MAP s -> lowest e s.t. s = eH
-    uint64_t* SM = calloc(1ULL << (n-k), sizeof(uint64_t));
-    CHECK_MALLOC(SM);
-
-    uint64_t count = 0, total = 0;
+int create_syndrome_table(uint64_t* SM, mzd_t* Ht, uint64_t* Httab1, uint64_t* Httab2, uint64_t n, uint64_t k) {
+    
+    printf("Creating syndrome table...\n");
+    uint64_t count = 0, total = 0, t, old, w, syn, nbiter, i, j, changes;
 
     // Create all linear combinations of i rows of H, with i=1..n-k-1
-    for (uint64_t i = 1; i < n; i++) { 
+    for (i = 1; i < n; i++) { 
 
-        uint64_t w = (1ULL << i) - 1; // i first bits to 1
-        uint64_t syn = Ht2tab[w >> n/2] ^ Ht1tab[w & ((1 << n/2) - 1)];
+        w = (1ULL << i) - 1; // i first bits to 1
+        syn = Httab2[w >> n/2] ^ Httab1[w & ((1 << n/2) - 1)];
         
-        uint64_t nbiter = binomial(n, i);
-        printf("i = %ld, nbiter = %lu\n", i, nbiter);
-        for (uint64_t j = 0 ; j < nbiter; j++) {
+        nbiter = binomial(n, i);
+        printf("wt(e) = %ld, nb possibilities = %lu\n", i, nbiter);
+        for (j = 0 ; j < nbiter; j++) {
             total++; 
             if (SM[syn] == 0) {
                 count++;
@@ -126,7 +107,7 @@ int main(void) {
                 w = (t + 1) | (((~t & -~t) - 1) >> (__builtin_ctz(old) + 1));  
                 
                 // Get the 2 indices that changed from last iteration
-                uint64_t changes = old ^ w;
+                changes = old ^ w;
                 // trailing zeroes, starting at LSB
                 int i1 = __builtin_ctz(changes);
                 // trailing zeroes, starting at MSB
@@ -143,18 +124,58 @@ int main(void) {
 
     if (count != (1ULL << (n-k))) {
         printf("Exhausted 2^n vectors and still have not found all syndromes O_o. Count is %lu\n", count);
-    } else {
-        printf("Syndrome table generated correctly! %lu total error vector for %lu syndromes\n", total, count);
-    }
-
-
-
-
+        return 0;
+    } 
     
+    printf("Done.\n");
+    return 1;
+}
+
+mzd_t* get_random_fullrank(int r, int c) {
+
+    mzd_t* M = mzd_init(r, c);
+    mzd_t* M2 = mzd_init(r, c);
+
+    do {
+        mzd_randomize(M);
+        mzd_copy(M2, M);
+    } while (mzd_echelonize(M2, 0) != MIN(r,c));
+    
+    mzd_free(M2);
+    return M;
+}
+int main(void) {
+    
+    uint64_t n = 40, k = 20;
+
+    mzd_t* Ink = mzd_init(n-k,n-k);
+    // n-k identity matrix
+    for (int i = 0; i < n-k; i++) mzd_write_bit(Ink, i, i, 1);
+    
+    mzd_t* M = get_random_fullrank(k, n-k);
+    mzd_t* H = mzd_concat(NULL,  M, Ink); // H = [M | I_{n-k}]
+    mzd_t* Ht = mzd_transpose(NULL, H);
+
+    uint64_t *Httab1 = malloc(sizeof(uint64_t) * (1ULL << n/2));
+    uint64_t *Httab2 = malloc(sizeof(uint64_t) * (1ULL << n/2));
+    CHECK_MALLOC(Httab1); CHECK_MALLOC(Httab2);
+
+    // MAP s -> lowest e s.t. s = eH^t
+    uint64_t* SM = calloc(1ULL << (n-k), sizeof(uint64_t));
+    CHECK_MALLOC(SM);
+    
+    tabulate_matrix(Ht, Httab1, Httab2, n, k);
+    test_matrix_tabulation(Ht, Httab1, Httab2, n, k);
+
+    create_syndrome_table(SM, Ht, Httab1, Httab2, n, k);
+    test_syndrome_table(SM, Httab1, Httab2, n, k);
+
+    printf("Syndrome table generated, all okay.\n");
+
     mzd_free(H);
     mzd_free(Ht);
-    free(Ht1tab);
-    free(Ht2tab);
+    free(Httab1);
+    free(Httab2);
     free(SM);
 
     return 0;

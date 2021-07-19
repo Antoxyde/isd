41e221c43be80ed5ae73ae2a4adda7cd852f22a5
antox <git@w0nderland.fr>
2021-07-19 15:04:16 +0200

    old upd scripts

--- scripts/graph-semibc.py
+++ scripts/graph-semibc.py
@@ -4,7 +4,7 @@ n,k = 1280,640
 GV = 144
 p = 2
 l = 20
-m = 5
+m = 1
 
 """
 prange = lambda w : sum(k * comb(n-k, i-1) for i in range(GV, w+1)) / 2**(n-k)
@@ -14,7 +14,8 @@ semibc = lambda w : ((l+1) * comb(k//2, p)**2 * sum(comb(n-k-l, i-2*p) for i in 
 # Esperances de nb de mots de poids w sur une itération
 prange = lambda w : k * comb(n-k, w-1) / 2**(n-k)
 #stern = lambda w : (comb(k//2, p)**2 * comb(n-k-l, w-2*p)) / 2**(n-k)
-stern = lambda w : (comb(k//2, p)**2)* (comb(n-k-l, w-2*p) / 2**(n-k))
+
+stern = lambda w : (comb(k//2, p)**2) * (comb(n-k-l, w-2*p) / 2**(n-k))
 
 stern_tweaked = lambda w : (m * comb(k//2, p)**2) * (comb(n-k-l, w-2*p) / 2**(n-k))
 semibc = lambda w : ( comb(k//2, p)**2 * (l * comb(n-k-l, w-2*p-1)  + comb(n-k-l, w-2*p) )) / 2**(n-k)
@@ -22,9 +23,27 @@ semibc = lambda w : ( comb(k//2, p)**2 * (l * comb(n-k-l, w-2*p-1)  + comb(n-k-l
 # Stern : l=18, p=2 basique, c-c = 
 # Stern tweaked : l=18, p=2, nwin = 5, c-c = 32
 
-for w in range(215, 230, 1):
-    print("w : {}".format(w))
-    print("- Prange : proba=2^{}, 48h expectation=2^{}".format(log(prange(w), 2), log(prange(w) * 172_800 * 182000 , 2)))
-    print("- Stern : proba=2^{},  48h expectation=2^{}".format(log(stern(w), 2), log(stern(w) * 172_800 * 1060, 2)))
-    print("- Stern tweaked : proba=2^{}, 48h expectation=2^{}".format(log(stern_tweaked(w), 2), log(stern_tweaked(w) * 172_800 * 500, 2)))
-    print("- SemiBC : proba=2^{}, 48h expectation=2^{}".format(log(semibc(w), 2), log(semibc(w) * 172_800 * 158, 2)))
+pts_stern_x, pts_sbc_x = [],[]
+pts_stern_y, pts_sbc_y = [],[]
+for w in range(200, 250, 1):
+    
+    t = 1/(stern_tweaked(w) * 1750) 
+    pts_stern_x.append(w)
+    pts_stern_y.append(log(t, 2))
+    t = 1/(semibc(w) * 158) 
+
+    pts_sbc_x.append(w)
+    pts_sbc_y.append(log(t, 2))
+
+
+import matplotlib.pyplot as plt
+
+plt.plot(pts_stern_x, pts_stern_y, color="red", label="Stern's algorithm")
+plt.plot(pts_sbc_x, pts_sbc_y, color="blue", label="Semi-BC algorithm")
+
+plt.xlabel('Weight', fontsize=25)
+plt.ylabel('log2 expected running time in seconds', fontsize=25)
+plt.legend(fontsize=20)
+plt.xticks(fontsize = 15)
+plt.yticks(fontsize = 15)
+plt.show()

5ab155fabd7e08e4338581e349b25646d7b30791
antox <git@w0nderland.fr>
2021-07-19 14:26:09 +0200

    comments

--- stern.c
+++ stern.c
@@ -13,7 +13,7 @@ mzd_t* stern(mzd_t* G, uint64_t time_sec) {
     double elapsed = 0.0;
     uint64_t iter = 0, delta = 0, cc_iter = 0, total = 0,  idx_win = 0, i = 0, j = 0;
     
-    // Combination structure for binomial combination generation
+    // Combination structure used to generate the pX choose k/2 rows combinations
     comb_t comb_struct;
     comb_diff_t comb_diff;
     
@@ -27,11 +27,15 @@ mzd_t* stern(mzd_t* G, uint64_t time_sec) {
 #if defined(DEBUG)
     uint64_t nb_collisions = 0, nb_collisions_delta = 0, collisions_list_size = 0;
 #endif /* DEBUG */
-
+    
+    // Holds the combination we are curretnly building when merging L1 and L2
     uint64_t* linear_comb = (uint64_t*)malloc(sizeof(uint64_t) * 10 /* K/64 */);
     uint64_t* linear_comb_next = (uint64_t*)malloc(sizeof(uint64_t) * 10 /* K/64 */);
+    // Store minimal codeword found up to this ponit
     uint64_t* min_cw = (uint64_t*)malloc(sizeof(uint64_t) * 10 /* K/64 */);
+    // Keep the column permutation to find back the min_cw at the end
     rci_t* column_perms_copy =  (rci_t*) malloc(sizeof(rci_t) * N);
+    // Hold the current state of columns permutations
     rci_t* column_perms = (rci_t*) malloc(sizeof(rci_t) * N);
     CHECK_MALLOC(linear_comb);
     CHECK_MALLOC(linear_comb_next);
@@ -48,8 +52,7 @@ mzd_t* stern(mzd_t* G, uint64_t time_sec) {
     rref_to_systematic(Gtemp, column_perms);
     mzd_t* Glw = mzd_submatrix(NULL, Gtemp, 0, K, K, N);
 
-    // nelem is the max number of LCs we will make
-    // so its an upper bound for the size of each array
+    // nelem_pX is the max number of LCs we will make in LX, so its an upper bound for the size of the corresponding array
     uint64_t nelem_p1 = binomial(320 /*K/2*/, P1);
     uint64_t nelem_p2 = binomial(320 /*K/2*/, P2);
 
@@ -57,7 +60,7 @@ mzd_t* stern(mzd_t* G, uint64_t time_sec) {
     uint64_t nb_keys_bits = 1ULL << (L - 3);
 
 #if defined(FILTER)
-    // Used to track which combination are present
+    // Used to track which combination are present / filter out non-colliding elements before the sort phase
     uint64_t** collisions_first_pass = (uint64_t**)malloc( M * sizeof(uint64_t*));
     uint64_t** collisions_second_pass = (uint64_t**)malloc( M * sizeof(uint64_t*));
     CHECK_MALLOC(collisions_first_pass);
@@ -65,6 +68,7 @@ mzd_t* stern(mzd_t* G, uint64_t time_sec) {
 #endif /* FILTER */
 
     // Holds the linear combinations made for each pass
+    // We need a sorted version as radixsort does not sort in place
     lc_tab** L2s = (lc_tab**)malloc(M * sizeof(lc_tab*));
     lc_tab** L1s = (lc_tab**)malloc(M * sizeof(lc_tab*));
     lc_tab** L2s_sorted = (lc_tab**)malloc(M * sizeof(lc_tab*));
@@ -74,7 +78,8 @@ mzd_t* stern(mzd_t* G, uint64_t time_sec) {
     CHECK_MALLOC(L1s);
     CHECK_MALLOC(L2s_sorted);
     CHECK_MALLOC(L1s_sorted);
-
+    
+    // Allocate stuff that  needs to be duplicated for each window
     for (idx_win = 0; idx_win < M; idx_win++) {
 
 #if defined(FILTER)
@@ -84,16 +89,16 @@ mzd_t* stern(mzd_t* G, uint64_t time_sec) {
         CHECK_MALLOC(collisions_second_pass[idx_win]);
 #endif /* FILTER */
 
-        L2s[idx_win] = malloc(sizeof(lc_tab));
-        L1s[idx_win] = malloc(sizeof(lc_tab));
-        L2s_sorted[idx_win] = malloc(sizeof(lc_tab));
-        L1s_sorted[idx_win] = malloc(sizeof(lc_tab));
-
+        L2s[idx_win] = (lc_tab*)malloc(sizeof(lc_tab));
+        L1s[idx_win] = (lc_tab*)malloc(sizeof(lc_tab));
+        L2s_sorted[idx_win] = (lc_tab*)malloc(sizeof(lc_tab));
+        L1s_sorted[idx_win] = (lc_tab*)malloc(sizeof(lc_tab));
         CHECK_MALLOC(L2s[idx_win]);
         CHECK_MALLOC(L1s[idx_win]);
         CHECK_MALLOC(L2s_sorted[idx_win]);
         CHECK_MALLOC(L1s_sorted[idx_win]);
         
+        // Init the lc_tab struct for each 4 holders
         L2s[idx_win]->p  = P2;
         L1s[idx_win]->p = P1;
         L2s_sorted[idx_win]->p  = P2;
@@ -115,12 +120,7 @@ mzd_t* stern(mzd_t* G, uint64_t time_sec) {
         CHECK_MALLOC(L1s_sorted[idx_win]->lcs);
     }
 
-    // A generic holder for the current indexes
-    uint64_t* lc_indexes = (uint64_t*)malloc(M * sizeof(uint64_t));
-    CHECK_MALLOC(lc_indexes);
-
-    // Generic alias on the sorted arrays
-
+    // Generic alias on the sorted arrays (radixsort might mix up the LXs and LXs_sorted pointers so we use an alias to avoid losing reference to one or the other).
     lc_tab** L2s_alias_sorted = (lc_tab**)malloc(M * sizeof(lc_tab*));
     lc_tab** L1s_alias_sorted = (lc_tab**)malloc(M * sizeof(lc_tab*));
 
@@ -136,15 +136,14 @@ mzd_t* stern(mzd_t* G, uint64_t time_sec) {
     uint16_t* combinations_p2 = (uint16_t*)malloc((P2 + 1) * sizeof(uint16_t));
     CHECK_MALLOC(combinations_p1);
     CHECK_MALLOC(combinations_p2);
-
-
+    
+    // Infinite loop, break when time has exceeded
     while (1) {
 
-        /* Start of the Canteaut-Chabaud stuff, to derive a new information set. */
         for (cc_iter = 0; cc_iter < CC; cc_iter++) {
             canteaut_chabaud(Glw, column_perms);
         }
-        /* End of the Canteaut-Chabaud stuff. We now have a proper Iset to work with. */
+        /* End of the Canteaut-Chabaud stuff. We now have a proper new Glw to work with. */
         
         // Reset previous iteration stuff
         for (idx_win = 0; idx_win < M; idx_win++) {
@@ -160,6 +159,7 @@ mzd_t* stern(mzd_t* G, uint64_t time_sec) {
             L1s_sorted[idx_win]->current_size = 0;
         }
 
+        // If filtering is enabled, starts by checking which elements are present in L1
 #if defined(FILTER)
         // 1st pass
         memset(combinations_p1, 0, (P1 + 1) * sizeof(uint16_t));
@@ -181,7 +181,8 @@ mzd_t* stern(mzd_t* G, uint64_t time_sec) {
         }
 #endif /* FILTER */
         
-        // 2nd pass
+        // If filtering is enabled, creates L2 by only keeping combinations that will collide with some L1 elements
+        // Otherwise, put all possible combinations in L2
         memset(combinations_p2, 0, (P2 + 1) * sizeof(uint16_t));
         init_combination(&comb_struct,combinations_p2, P2, 320 /*K/2*/);
         for (i = 0; i < nelem_p2; i++) {
@@ -211,7 +212,8 @@ mzd_t* stern(mzd_t* G, uint64_t time_sec) {
             next_combination(&comb_struct, &comb_diff);
         }
         
-        // 3rd pass
+        // If filtering is enabled, creates L1 by only keeping combinations that will collide with some L2 elements
+        // Otherwise, put all possible combinations in L1
         memset(combinations_p1, 0, (P1 + 1) * sizeof(uint16_t));
         init_combination(&comb_struct,combinations_p1, P1, 320 /*K/2*/);
         for (i = 0; i < nelem_p1; i++) {
@@ -237,27 +239,25 @@ mzd_t* stern(mzd_t* G, uint64_t time_sec) {
             }
             next_combination(&comb_struct, &comb_diff);
         }
+        
 
-        /* From here, L1s[i][:L1s_size[i]] and L2s[i][:L2s_size[i]]
-        *  contains the LCs that WILL collide.
-        * So we sort em all. */
-
-
+        // Sort L1 and L2
         for (idx_win = 0; idx_win < M; idx_win++) {
             
             L2s_sorted[idx_win]->current_size = L2s[idx_win]->current_size;
             L1s_sorted[idx_win]->current_size = L1s[idx_win]->current_size;
 
             // We have to use aliases to avoid mixing up the LXs_sorted and LXs pointers (radixsort returns one or the other, depending on the radix_nlen parameter.)
-            L2s_alias_sorted[idx_win] = radixsort(L2s[idx_win], L2s_sorted[idx_win], L2s[idx_win]->current_size, aux);
-            L1s_alias_sorted[idx_win] = radixsort(L1s[idx_win], L1s_sorted[idx_win], L1s[idx_win]->current_size, aux);
+            L2s_alias_sorted[idx_win] = radixsort(L2s[idx_win], L2s_sorted[idx_win], aux);
+            L1s_alias_sorted[idx_win] = radixsort(L1s[idx_win], L1s_sorted[idx_win], aux);
         }
         
         // Now that everything is sorted,  we can actually match all the pairs to get the collisions.
         for (idx_win = 0; idx_win < M; idx_win++) {
 
             int index_second = 0, index_third = 0, save_index_third = 0;
-
+                
+            // For each elements in L2, sorted
             for (index_second = 0; index_second < L2s_alias_sorted[idx_win]->current_size; index_second++) {
                 
                 if (index_second > 0 && LC_TAB_GET(L2s_alias_sorted[idx_win],index_second - 1)->delta != LC_TAB_GET(L2s_alias_sorted[idx_win],index_second)->delta) {
@@ -281,6 +281,8 @@ mzd_t* stern(mzd_t* G, uint64_t time_sec) {
                     mxor(linear_comb, (uint64_t*)Glw->rows[LC_TAB_GET(L2s_alias_sorted[idx_win], index_second)->indexes[i] + 320], 10 /* K/64 */);
                 }
 #endif /* AVX512_ENABLED */
+
+                // For each element in L1, sorted
                 for (index_third = save_index_third; index_third < L1s_alias_sorted[idx_win]->current_size && LC_TAB_GET(L2s_alias_sorted[idx_win], index_second)->delta >= LC_TAB_GET(L1s_alias_sorted[idx_win], index_third)->delta; index_third++) {
 
                     if (LC_TAB_GET(L2s_alias_sorted[idx_win], index_second)->delta > LC_TAB_GET(L1s_alias_sorted[idx_win], index_third)->delta) continue;
@@ -313,7 +315,8 @@ mzd_t* stern(mzd_t* G, uint64_t time_sec) {
                         mxor(linear_comb_next, (uint64_t*)Glw->rows[LC_TAB_GET(L1s_alias_sorted[idx_win],index_third)->indexes[i]], 10 /* K/64 */);
                     }
 #endif /* AVX512_ENABLED */
-
+                    
+                    // Compute the weight of half of the codeword (only redundant part)
                     wt = popcnt64_unrolled(linear_comb_next, 10 /* K/64 */);
                     total++;
                     if (wt < min_wt) {
@@ -329,13 +332,14 @@ mzd_t* stern(mzd_t* G, uint64_t time_sec) {
                         for (i = 0; i < P2; i++) {
                             min_comb[i] += 320;
                         }
-
                         memcpy(min_comb + P2, LC_TAB_GET(L1s_alias_sorted[idx_win], index_third)->indexes, P1 * sizeof(uint16_t));
-
+                        
+                        // Save the new minimum codeword found
                         memcpy(min_cw, linear_comb_next, 80 /* K/8 */);
+                        // And the correponsping permutation state
                         memcpy(column_perms_copy, column_perms, N * sizeof(rci_t));
                         
-  
+                        // Reconstruct it and print it
                         mzd_t* cw = stern_reconstruct_cw(min_comb, column_perms_copy, min_cw, P1 + P2);
                         print_cw(cw);
                         mzd_free(cw);
@@ -349,7 +353,8 @@ mzd_t* stern(mzd_t* G, uint64_t time_sec) {
         iter++;
         current = clock();
         elapsed = ((double)(current - start))/CLOCKS_PER_SEC;
-
+        
+        // If time's up, leave
         if (elapsed > time_sec) {
             break;
         }
@@ -374,8 +379,6 @@ mzd_t* stern(mzd_t* G, uint64_t time_sec) {
     free(column_perms);
 
     for (idx_win = 0; idx_win < M; idx_win++) {
-
-
         free(L2s[idx_win]->lcs);
         free(L1s[idx_win]->lcs);
         free(L2s_sorted[idx_win]->lcs);
@@ -390,7 +393,6 @@ mzd_t* stern(mzd_t* G, uint64_t time_sec) {
     free(L2s_alias_sorted);
     free(L1s_alias_sorted);
 
-    free(lc_indexes);
     free(aux);
 
 #if defined(FILTER)
@@ -407,17 +409,21 @@ mzd_t* stern(mzd_t* G, uint64_t time_sec) {
     return result;
 }
 
-
-lc_tab* denomsort_r(lc_tab* T, lc_tab* Ts, int64_t Tlen, uint64_t width, uint64_t pos, uint32_t* Aux) {
+/* Counting sort, as a subroutine for radix sort 
+ * T: Input array
+ * Ts: output array
+ * pos: current position to sort by 
+ * Aux: counting array */
+lc_tab* denomsort_r(lc_tab* T, lc_tab* Ts, uint64_t pos, uint32_t* Aux) {
 
     uint32_t mask, k;
     int64_t i;
-    k = 1UL << width;
+    k = 1UL << RADIX_WIDTH;
     mask = k - 1;
 
     memset(Aux, 0, k * sizeof(uint32_t));
 
-    for (i = 0; i < Tlen; i++) {
+    for (i = 0; i < T->current_size; i++) {
         Aux[ ( LC_TAB_GET(T, i)->delta >> pos) & mask]++;
     }
 
@@ -425,7 +431,7 @@ lc_tab* denomsort_r(lc_tab* T, lc_tab* Ts, int64_t Tlen, uint64_t width, uint64_
         Aux[i] += Aux[i - 1];
     }
 
-    for (i = Tlen - 1; i >= 0; i--) {
+    for (i = T->current_size  - 1; i >= 0; i--) {
         uint32_t val = (LC_TAB_GET(T, i)->delta >> pos) & mask;
         Aux[val]--; 
         memcpy(LC_TAB_GET(Ts, Aux[val]), LC_TAB_GET(T, i), sizeof(lc) + T->p * sizeof(uint16_t));
@@ -434,14 +440,18 @@ lc_tab* denomsort_r(lc_tab* T, lc_tab* Ts, int64_t Tlen, uint64_t width, uint64_
     return Ts;
 }
 
-lc_tab* radixsort(lc_tab* T, lc_tab* Ts, int64_t Tlen, uint32_t* aux) {
+/* Sort T and write the result in Ts
+* T: Input array 
+* Ts: Output array
+* Aux: array used in denomsort, preallocated to avoid allocation at each call */
+lc_tab* radixsort(lc_tab* T, lc_tab* Ts, uint32_t* aux) {
 
     int i;
     lc_tab* tmp;
 
     for (i = 0; i < RADIX_LEN; i++) {
         tmp = T;
-        T = denomsort_r(T, Ts, Tlen, RADIX_WIDTH, i*RADIX_WIDTH, aux);
+        T = denomsort_r(T, Ts, i*RADIX_WIDTH, aux);
         Ts = tmp;
     }
 
--- stern.h
+++ stern.h
@@ -8,9 +8,9 @@
 #include "m4ri/m4ri.h"
 #include <stdint.h>
 
-#define M       1         // Number of window
-#define P1      2         // Stern's P parameter
-#define P2      2         // Stern's P parameter
+#define M       5         // Number of window
+#define P1      3         // Stern's P parameter
+#define P2      3         // Stern's P parameter
 #define N       1280      // Code length
 #define K       640       // Code dimension
 #define L       20        // Stern's L paramter, window size 
@@ -22,32 +22,40 @@
 #define STERN_GET(tab, index) ((tab[index >> 6]) >> (index & 0x3f)) & 1
 #define STERN_SET_ONE(tab, index) (tab[index >> 6]) |= (1ULL << (index & 0x3f))
 
+/* We can't have an array of struct with dynamically-sized in C without using malloc (Actually we can have `flexible array members` but not arrays of them)
+* Calling malloc for each of `PX choose K/2` instances is not a possibility
+* So we use a small hack by keeping the number of elements lc->indexes in a parent struct (lc_tab->p)
+* And convenients macros to use this weird structure
+* Admit its not really 
+*/
+
 // Represent an LC array
 typedef struct lc_tab_ {
-    uint8_t p;
-    size_t current_size;
-    void* lcs;
+    uint8_t p; // used to have a dynamic size for the elements of lcs
+    size_t current_size; // Current number of elements in lcs
+    void* lcs; // pointer to an array of lc*
 } lc_tab;
+
 
 // Represent a linear combination
 typedef struct lc_ {
-    uint32_t delta;
-    uint16_t indexes[];
+    uint32_t delta; // Value on the window for this combination
+    uint16_t indexes[]; // Row indexes to add to obtain the combination. Normalised to [0,K/2].
 } lc;
 
+// Convenient macros for the use of the above structures
 #define LC_TAB_GET(tab, i) ((lc*)((tab)->lcs + (i) * (sizeof(uint32_t) + sizeof(uint16_t) * (tab)->p)))
 #define LC_CALLOC(nelem, p) (lc*)(calloc(sizeof(uint32_t) + sizeof(uint16_t) * (p) , nelem))
 #define LC_MALLOC(nelem, p) (lc*)(malloc((sizeof(uint32_t) + sizeof(uint16_t) * (p)) * nelem))
 
 /* Input:
  *  - G, a n/2 x n generator matrix
- *  - 
+ *  - time_sec, number of seconds to run the algorithm
  * Output:
  * - min_cw, the lowest codeword found
  */
- mzd_t* stern(mzd_t* G, uint64_t niter);
-int compare_lc(const void* a, const void* b);
-lc_tab* denomsort_r(lc_tab* T, lc_tab* Ts, int64_t Tlen, uint64_t width, uint64_t pos, uint32_t* aux);
-lc_tab* radixsort(lc_tab* T, lc_tab* Ts, int64_t Tlen, uint32_t *aux);
+ mzd_t* stern(mzd_t* G, uint64_t time_sec);
+lc_tab* denomsort_r(lc_tab* T, lc_tab* Ts, uint64_t pos, uint32_t* aux);
+lc_tab* radixsort(lc_tab* T, lc_tab* Ts, uint32_t *aux);
 
 #endif
